3ab8a3c8cd1ccba94e7459a22acc46099361aaa5
Naiery <121600934+Naiery0@users.noreply.github.com>
2026-05-14 14:50:40 +0900

    초기 커밋.

---
+++ Winform_ToyProject/Uc_Piano.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Winform_ToyProject
+{
+    partial class Uc_Piano
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn_PnC = new DevExpress.XtraEditors.SimpleButton();
+            btn_PnD = new DevExpress.XtraEditors.SimpleButton();
+            btn_PnE = new DevExpress.XtraEditors.SimpleButton();
+            btn_PnF = new DevExpress.XtraEditors.SimpleButton();
+            btn_PnG = new DevExpress.XtraEditors.SimpleButton();
+            btn_PnB = new DevExpress.XtraEditors.SimpleButton();
+            btn_PnA = new DevExpress.XtraEditors.SimpleButton();
+            btn_PnCs = new DevExpress.XtraEditors.SimpleButton();
+            btn_PnDs = new DevExpress.XtraEditors.SimpleButton();
+            btn_PnFs = new DevExpress.XtraEditors.SimpleButton();
+            btn_PnGS = new DevExpress.XtraEditors.SimpleButton();
+            btn_PnAs = new DevExpress.XtraEditors.SimpleButton();
+            SuspendLayout();
+            // 
+            // btn_PnC
+            // 
+            btn_PnC.Appearance.BackColor = Color.White;
+            btn_PnC.Appearance.Options.UseBackColor = true;
+            btn_PnC.Dock = DockStyle.Left;
+            btn_PnC.Location = new Point(0, 0);
+            btn_PnC.Name = "btn_PnC";
+            btn_PnC.Size = new Size(95, 451);
+            btn_PnC.TabIndex = 0;
+            btn_PnC.Text = " ";
+            btn_PnC.Click += PianoTileClick;
+            // 
+            // btn_PnD
+            // 
+            btn_PnD.Appearance.BackColor = Color.White;
+            btn_PnD.Appearance.Options.UseBackColor = true;
+            btn_PnD.Dock = DockStyle.Left;
+            btn_PnD.Location = new Point(95, 0);
+            btn_PnD.Name = "btn_PnD";
+            btn_PnD.Size = new Size(95, 451);
+            btn_PnD.TabIndex = 1;
+            btn_PnD.Text = " ";
+            btn_PnD.Click += PianoTileClick;
+            // 
+            // btn_PnE
+            // 
+            btn_PnE.Appearance.BackColor = Color.White;
+            btn_PnE.Appearance.Options.UseBackColor = true;
+            btn_PnE.Dock = DockStyle.Left;
+            btn_PnE.Location = new Point(190, 0);
+            btn_PnE.Name = "btn_PnE";
+            btn_PnE.Size = new Size(95, 451);
+            btn_PnE.TabIndex = 2;
+            btn_PnE.Text = " ";
+            btn_PnE.Click += PianoTileClick;
+            // 
+            // btn_PnF
+            // 
+            btn_PnF.Appearance.BackColor = Color.White;
+            btn_PnF.Appearance.Options.UseBackColor = true;
+            btn_PnF.Dock = DockStyle.Left;
+            btn_PnF.Location = new Point(285, 0);
+            btn_PnF.Name = "btn_PnF";
+            btn_PnF.Size = new Size(95, 451);
+            btn_PnF.TabIndex = 3;
+            btn_PnF.Text = " ";
+            btn_PnF.Click += PianoTileClick;
+            // 
+            // btn_PnG
+            // 
+            btn_PnG.Appearance.BackColor = Color.White;
+            btn_PnG.Appearance.Options.UseBackColor = true;
+            btn_PnG.Dock = DockStyle.Left;
+            btn_PnG.Location = new Point(380, 0);
+            btn_PnG.Name = "btn_PnG";
+            btn_PnG.Size = new Size(95, 451);
+            btn_PnG.TabIndex = 4;
+            btn_PnG.Text = " ";
+            btn_PnG.Click += PianoTileClick;
+            // 
+            // btn_PnB
+            // 
+            btn_PnB.Appearance.BackColor = Color.White;
+            btn_PnB.Appearance.Options.UseBackColor = true;
+            btn_PnB.Dock = DockStyle.Left;
+            btn_PnB.Location = new Point(570, 0);
+            btn_PnB.Name = "btn_PnB";
+            btn_PnB.Size = new Size(95, 451);
+            btn_PnB.TabIndex = 6;
+            btn_PnB.Text = " ";
+            btn_PnB.Click += PianoTileClick;
+            // 
+            // btn_PnA
+            // 
+            btn_PnA.Appearance.BackColor = Color.White;
+            btn_PnA.Appearance.Options.UseBackColor = true;
+            btn_PnA.Dock = DockStyle.Left;
+            btn_PnA.Location = new Point(475, 0);
+            btn_PnA.Name = "btn_PnA";
+            btn_PnA.Size = new Size(95, 451);
+            btn_PnA.TabIndex = 5;
+            btn_PnA.Text = " ";
+            btn_PnA.Click += PianoTileClick;
+            // 
+            // btn_PnCs
+            // 
+            btn_PnCs.Appearance.BackColor = Color.Black;
+            btn_PnCs.Appearance.Options.UseBackColor = true;
+            btn_PnCs.Location = new Point(60, 0);
+            btn_PnCs.Name = "btn_PnCs";
+            btn_PnCs.Size = new Size(68, 313);
+            btn_PnCs.TabIndex = 8;
+            btn_PnCs.Text = " ";
+            btn_PnCs.Click += PianoTileClick;
+            // 
+            // btn_PnDs
+            // 
+            btn_PnDs.Appearance.BackColor = Color.Black;
+            btn_PnDs.Appearance.Options.UseBackColor = true;
+            btn_PnDs.Location = new Point(155, 0);
+            btn_PnDs.Name = "btn_PnDs";
+            btn_PnDs.Size = new Size(68, 313);
+            btn_PnDs.TabIndex = 9;
+            btn_PnDs.Text = " ";
+            btn_PnDs.Click += PianoTileClick;
+            // 
+            // btn_PnFs
+            // 
+            btn_PnFs.Appearance.BackColor = Color.Black;
+            btn_PnFs.Appearance.Options.UseBackColor = true;
+            btn_PnFs.Location = new Point(345, 0);
+            btn_PnFs.Name = "btn_PnFs";
+            btn_PnFs.Size = new Size(68, 313);
+            btn_PnFs.TabIndex = 10;
+            btn_PnFs.Text = " ";
+            btn_PnFs.Click += PianoTileClick;
+            // 
+            // btn_PnGS
+            // 
+            btn_PnGS.Appearance.BackColor = Color.Black;
+            btn_PnGS.Appearance.Options.UseBackColor = true;
+            btn_PnGS.Location = new Point(441, 0);
+            btn_PnGS.Name = "btn_PnGS";
+            btn_PnGS.Size = new Size(68, 313);
+            btn_PnGS.TabIndex = 11;
+            btn_PnGS.Text = " ";
+            btn_PnGS.Click += PianoTileClick;
+            // 
+            // btn_PnAs
+            // 
+            btn_PnAs.Appearance.BackColor = Color.Black;
+            btn_PnAs.Appearance.Options.UseBackColor = true;
+            btn_PnAs.Location = new Point(536, 0);
+            btn_PnAs.Name = "btn_PnAs";
+            btn_PnAs.Size = new Size(68, 313);
+            btn_PnAs.TabIndex = 12;
+            btn_PnAs.Text = " ";
+            btn_PnAs.Click += PianoTileClick;
+            // 
+            // Uc_Piano
+            // 
+            Appearance.BackColor = Color.Transparent;
+            Appearance.Options.UseBackColor = true;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btn_PnAs);
+            Controls.Add(btn_PnGS);
+            Controls.Add(btn_PnFs);
+            Controls.Add(btn_PnDs);
+            Controls.Add(btn_PnCs);
+            Controls.Add(btn_PnB);
+            Controls.Add(btn_PnA);
+            Controls.Add(btn_PnG);
+            Controls.Add(btn_PnF);
+            Controls.Add(btn_PnE);
+            Controls.Add(btn_PnD);
+            Controls.Add(btn_PnC);
+            Name = "Uc_Piano";
+            Size = new Size(664, 451);
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton btn_PnC;
+        private DevExpress.XtraEditors.SimpleButton btn_PnD;
+        private DevExpress.XtraEditors.SimpleButton btn_PnE;
+        private DevExpress.XtraEditors.SimpleButton btn_PnF;
+        private DevExpress.XtraEditors.SimpleButton btn_PnG;
+        private DevExpress.XtraEditors.SimpleButton btn_PnB;
+        private DevExpress.XtraEditors.SimpleButton btn_PnA;
+        private DevExpress.XtraEditors.SimpleButton btn_PnCs;
+        private DevExpress.XtraEditors.SimpleButton btn_PnDs;
+        private DevExpress.XtraEditors.SimpleButton btn_PnFs;
+        private DevExpress.XtraEditors.SimpleButton btn_PnGS;
+        private DevExpress.XtraEditors.SimpleButton btn_PnAs;
+    }
+}
